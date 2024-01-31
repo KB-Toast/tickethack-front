bb1e1b6e8b52a5e6bd728777c0b18bd896e82f30
KB-Toast <kevin.brusson@gmail.com>
2024-01-31 09:50:44 +0100

    playing with dates

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-function searchTrip() {
+async function getSingleTrip(trips) {
+  for (let trip of trips) {
+    // new fetch for every  single trip found
+    console.log(trip);
+    const singleResult = await fetch(
+      `http://localhost:3000/trips/search/${trip._id}`
+    );
+    const allResultItems = await singleResult.json();
+    let singleTrip = `
+        <div class="single-trip" id="${allResultItems.trip._id}">
+          <div class="travel-trip">
+              ${allResultItems.trip.departure} > ${allResultItems.trip.arrival}
+          </div>
+          <div class="hour-trip">
+              ${allResultItems.trip.date}
+          </div>
+          <div class="price-trip">
+              ${allResultItems.trip.price}€
+          </div>
+          <button class="remove-trip">
+              X
+          </button>
+        </div>
+        `;
+
+    document.querySelector('#content-right').innerHTML += singleTrip;
+  }
+}
+
+async function searchTrip() {
   const departureValue = document.querySelector('#departure').value;
   const arrivalValue = document.querySelector('#arrival').value;
   const dateValue = document.querySelector('#dateSelection').value;
@@ -12,23 +41,25 @@ function searchTrip() {
 
     //console.log(userInfos);
 
-    fetch('http://localhost:3000/trips/search', {
+    const AllResults = await fetch('http://localhost:3000/trips/search', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(userInfos),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        if (data.trips.length === 0) {
-          document.querySelector('#img-train').src = 'images/notfound.png';
-          document.querySelector('#text-result').textContent = 'No trip found.';
-        } else {
-          // add all found trip to right part of page
-        }
-      });
+    });
+
+    const allResultItems = await AllResults.json();
+    console.log(allResultItems);
+    if (allResultItems.trips.length === 0) {
+      document.querySelector('#img-train').src = 'images/notfound.png';
+      document.querySelector('#text-result').textContent = 'No trip found.';
+    } else {
+      // removing right part content
+      document.querySelector('#content-right').innerHTML = '';
+      // add all found trip to right part of page
+      const singleTrip = await getSingleTrip(allResultItems.trips);
+    }
   } else {
     console.log('1 or more missing fields');
   }
